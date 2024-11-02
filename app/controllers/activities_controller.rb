4a7568312e3f935b9ce0f0ad5c34ad0019835b53
“gretaHoogstra” <ghoogstra@colgate.edu>
2024-11-02 13:22:48 -0400

    added destroy

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -6,4 +6,11 @@ class ActivitiesController < ApplicationController
     def show
       @activity = Activity.find(params[:id])
     end
+
+    def destroy #this should probably have permissions attached to it in some way eventually
+      @activity = Activity.find(params[:id])
+      @activity.destroy
+      flash[:notice] = "activity removed"
+      redirect_to activities_path
+    end
 end
