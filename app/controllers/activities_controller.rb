5cdbfb5cef313d92a75587e3fb44bc87f4e17c67
“juliagoosay” <jgoosay@colgate.edu>
2024-11-02 13:05:10 -0400

    Search bar implemented (sorting still a work in progress)

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,12 @@
 class ActivitiesController < ApplicationController
     def index
       @activities = Activity.all.order(:title)
+      if params[:query].present? && params[:query].length > 2
+        @activities = @activities.by_search_string(params[:query])
+      end
+      if params[:opennow].present? && params[:opennow].to_i == 1
+        @activities = @activities.open_now
+      end
     end
 
     def show
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,7 @@
 class Activity < ApplicationRecord
     enum :day, %i[Monday Tuesday Wednesday Thursday Friday]
 
+    def self.by_search_string(search)
+        Activity.where("description LIKE ?", "%#{search}%").or(Activity.where("title LIKE ?", "%#{search}%")).order(:day)
+    end
 end
