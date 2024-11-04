304d8446f610fe83fe90d21f08d5882e0fb33545
“juliagoosay” <jgoosay@colgate.edu>
2024-11-03 22:57:34 -0500

    Finished Filtering by Day

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,16 +1,21 @@
 class ActivitiesController < ApplicationController
     def index
+      if_clicked = false
       @activities = Activity.all.order(:title)
       if params[:query].present? && params[:query].length > 2
         @activities = @activities.by_search_string(params[:query])
       end
+      multi_day = Array.new
+      i=0
       Activity.days.keys.each do |key_str|
         sym_day = key_str
         if params[sym_day] != nil && params[sym_day] != "0"
+          if_clicked = true
           debugger
-          @activities = @activities.filter_by_day(sym_day)
+          multi_day.append(@activities.filter_by_day(sym_day))
         end
       end
+      @activities = multi_day.flatten if if_clicked == true
     end
 
     def show
