a983e72d9299929b8c09a1b3a309f2bae0be83f2
nfmccall21 <nfmccall@gmail.com>
2024-11-17 19:07:36 +0000

    refactoring methods in student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,16 +5,27 @@ class Student < ApplicationRecord
     has_and_belongs_to_many :users
 
     def enrolled_activities
-        Registration.where(student_id: id).where(status: :Enrolled)
+        #Registration.where(student_id: id).where(status: :Enrolled)
+        activities.joins(:registrations).merge(Registration.enrolled)
     end
 
     def waitlisted_activities
-        Registration.where(student_id: id).where(status: :Waitlist)
+        #Registration.where(student_id: id).where(status: :Waitlist)
+        activities.joins(:registrations).merge(Registration.waitlist)
     end
 
     def self.by_search_string(search)
+        # search_terms = search.split
+        # Student.where("firstname LIKE ?", "%#{search[0]}%").or(Student.where("lastname LIKE ?", "%#{search[1]}%"))
         search_terms = search.split
-        Student.where("firstname LIKE ?", "%#{search[0]}%").or(Student.where("lastname LIKE ?", "%#{search[1]}%"))
+
+        if search_terms.size == 1
+            where("firstname LIKE :term OR lastname LIKE :term", term: "%#{search_terms[0]}%")
+        elsif search_terms.size > 1
+            where("firstname LIKE :first AND lastname LIKE :last", first: "%#{search_terms[0]}%", last: "%#{search_terms[1]}%")
+        else
+            all
+        end
     end
 
 
